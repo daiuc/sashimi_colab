4b9ec6632b3c3c087cdf3b4f8621765b30abcc4f
daiuc <chaodai@uchicago.edu>
2023-08-11 14:33:10 -0500

    slight change to plot.sh

--- plot.sh
+++ plot.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-prep_region="chr10:99989193-100989193"
+data_pre_region="chr10:99989193-100989193"
 top_snp="chr10:99514301:T:C"
 
-plot_region="chr10:100300000-100400000"
+plot_region="chr10:100500000-100540000"
+counts=counts_clu_113382.txt
 
+vcf=ZOD14598_AD_GRM_WGS_2021-04-29_chr10.recalibrated_variants.leftnorm.filtered.AF.chr10_99489193_101489193.test_samples.rename.new.recode.vcf
+sample_id_list=individuals.txt
+
+clu=$(echo $counts | sed -n 's/[^_]*_\(.*\)\.txt/\1/p')
+out_pdf=sashimi_${clu}.pdf
 
 # data prep
 ./sashimi_ingredients.py \
-    -V ZOD14598_AD_GRM_WGS_2021-04-29_chr10.recalibrated_variants.leftnorm.filtered.AF.chr10_99489193_101489193.test_samples.rename.new.recode.vcf \
+    -V $vcf \
     -B <(realpath *.bw) \
-    -C counts_clu_109523.txt \
-    -P individuals.txt \
+    -C $counts \
+    -P $sample_id_list \
     -T template.ini \
     -S $top_snp \
-    -R $prep_region \
+    -R $data_pre_region \
     -E 1000 \
     -O ./plot \
     && echo "data prep done" \
@@ -22,7 +28,8 @@ plot_region="chr10:100300000-100400000"
     && /scratch/midway3/chaodai/miniconda3/envs/pygenometracks/bin/pyGenomeTracks \
             --tracks plot.ini \
             --region  $plot_region \
-            --title "QTL:${prep_region}|TOP:${top_snp}"\
+            --title "QTL:${plot_region}|TOP:${top_snp}|$clu"\
             --width 30 \
-            --trackLabelFraction .1 --fontSize 12 \
-            -out plot_sashimi.pdf
+            --trackLabelFraction .1 --fontSize 10 \
+            -out $out_pdf \
+    && echo "plot done"
